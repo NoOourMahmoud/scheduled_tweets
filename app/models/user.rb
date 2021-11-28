29625d41d2cadc5a54429164fa516151d60de3d5
NoOourMahmoud <eng.nourhanmahmoud@yahoo.com>
2021-11-28 15:52:35 +0200

    fix some bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-    has_many :twitter_accounts
-    has_many :tweets
+    has_many :twitter_accounts, dependent: :destroy
+    has_many :tweets, dependent: :destroy
     has_secure_password
+
+    validates :email, uniqueness: true
 end
